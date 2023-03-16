5f5baa1b712ca134a5a7b1801dbfc3e0acb83423
sohostory <sohostory@gmail.com>
2023-03-15 20:45:01 -0500

    added robot cards and search functionality

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
-import Hello from "./Hello";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello greeting={"Hello" + "React Ninja"} />
+    <App />
   </React.StrictMode>
 );
 
