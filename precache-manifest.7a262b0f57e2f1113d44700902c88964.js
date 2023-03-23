7441f973a1ce6539ece1219663b1d5a6c05f1600
sohostory <sohostory@gmail.com>
2023-03-22 19:17:53 -0500

    Updates

---
+++ precache-manifest.7a262b0f57e2f1113d44700902c88964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6631c3b5ebfa9300acf",
+    "url": "/robofriends/static/css/main.cb320d5b.chunk.css"
+  },
+  {
+    "revision": "c6631c3b5ebfa9300acf",
+    "url": "/robofriends/static/js/main.c6631c3b.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "e7c00086563453ddcb36",
+    "url": "/robofriends/static/css/2.b5c00d94.chunk.css"
+  },
+  {
+    "revision": "e7c00086563453ddcb36",
+    "url": "/robofriends/static/js/2.e7c00086.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bb8436e6ada337c36746843aeb6e8850",
+    "url": "/robofriends/index.html"
+  }
+];
