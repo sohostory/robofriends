eb4bd4af9fdbcc083dc00786683814309726b3aa
sohostory <sohostory@gmail.com>
2024-04-25 22:09:04 -0500

    updated state object with destructuring

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,4 @@
-import {
-  CHANGE_SEARCH_FIELD,
-  REQUEST_ROBOTS_PENDING,
-  REQUEST_ROBOTS_SUCCESS,
-  REQUEST_ROBOTS_FAILED,
-} from "./constants";
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from "./constants";
 
 const initialStateSearch = {
   searchField: "",
@@ -12,7 +7,7 @@ const initialStateSearch = {
 export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
-      return Object.assign({}, state, { searchField: action.payload });
+      return { ...state, searchField: action.payload };
     default:
       return state;
   }
@@ -26,17 +21,11 @@ const initialStateRobots = {
 export const requestRobots = (state = initialStateRobots, action = {}) => {
   switch (action.type) {
     case REQUEST_ROBOTS_PENDING:
-      return Object.assign({}, state, { isPending: true });
+      return { ...state, isPending: true };
     case REQUEST_ROBOTS_SUCCESS:
-      return Object.assign({}, state, {
-        robots: action.payload,
-        isPending: false,
-      });
+      return { ...state, robots: action.payload, isPending: false };
     case REQUEST_ROBOTS_FAILED:
-      return Object.assign({}, state, {
-        isPending: false,
-        error: action.payload,
-      });
+      return { ...state, isPending: false, error: action.payload };
     default:
       return state;
   }
