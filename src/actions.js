cc2fb3d0dc1e2004c9a16616ecc277f21800f693
sohostory <sohostory@gmail.com>
2024-04-25 21:53:18 -0500

    updated fetch call to async await method

--- src/actions.js
+++ src/actions.js
@@ -1,23 +1,17 @@
-import {
-  CHANGE_SEARCH_FIELD,
-  REQUEST_ROBOTS_PENDING,
-  REQUEST_ROBOTS_SUCCESS,
-  REQUEST_ROBOTS_FAILED,
-} from "./constants";
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from "./constants";
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text,
 });
 
-export const requestRobots = () => (dispatch) => {
-  dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-      dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
-    })
-    .catch((error) =>
-      dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error })
-    );
+export const requestRobots = () => async (dispatch) => {
+  try {
+    dispatch({ type: REQUEST_ROBOTS_PENDING });
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error });
+  }
 };
