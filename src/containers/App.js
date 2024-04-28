9501e51bf08ee3f2fb122998a26616ae56d69817
sohostory <sohostory@gmail.com>
2024-04-28 09:58:08 -0500

    fixed minor bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,7 +27,7 @@ const App = () => {
   ) : (
     <div className="tc">
       <Header />
-      <SearchBox searchChange={onSearchChange} />
+      <SearchBox />
       <Scroll>
         {isPending ? (
           <h1>Loading</h1>
