4d61bc93825cd35d2ce638a8795072e6bf479196
sohostory <sohostory@gmail.com>
2024-04-28 10:13:29 -0500

    added resister line to index file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import robotReducer from "./app/robotSlice";
 
 import "./index.css";
 import App from "./containers/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 import { configureStore } from "@reduxjs/toolkit";
@@ -26,6 +27,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
