e6c122341d0a866ca018bbd3475329d6b06bbe28
sohostory <sohostory@gmail.com>
2024-05-02 05:24:11 -0500

    added mockColor to the test

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -2,11 +2,14 @@ import { shallow } from "enzyme";
 import CounterButton from "./CounterButton";
 
 it("reders without crashing", () => {
-  expect(shallow(<CounterButton />)).toMatchSnapshot();
+  const mockColor = "red";
+  expect(shallow(<CounterButton color={mockColor} />)).toMatchSnapshot();
 });
 
 it("correctly increments the counter", () => {
-  const wrapper = shallow(<CounterButton />);
+  const mockColor = "red";
+  const wrapper = shallow(<CounterButton color={mockColor} />);
+
   expect(wrapper).toMatchSnapshot();
   wrapper.find('[id="counter"]').simulate("click");
   expect(wrapper.state()).toEqual({ count: 1 });
