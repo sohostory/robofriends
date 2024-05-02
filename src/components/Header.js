6f015e3aaaf23d3cccc9eb9048d2621ac29141c6
sohostory <sohostory@gmail.com>
2024-05-01 21:25:29 -0500

    Added CounterButton for stateful testing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import { memo } from "react";
+import CounterButton from "./CounterButton";
 
 const Header = memo(function Header() {
-  return <h1 className="f1">RoboFriends</h1>;
+  return (
+    <div>
+      <h1 className="f1">RoboFriends</h1>
+      <CounterButton />
+    </div>
+  );
 });
 
 export default Header;
